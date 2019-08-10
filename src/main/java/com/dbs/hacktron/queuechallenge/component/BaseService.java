6f9251a3d27aa639c54e0c94ba3813732822a114
krishna <mkp.iiit@gmail.com>
2019-08-10 01:07:21 -0700

    m2

--- src/main/java/com/dbs/hacktron/queuechallenge/component/BaseService.java
+++ src/main/java/com/dbs/hacktron/queuechallenge/component/BaseService.java
@@ -1,10 +1,13 @@
 package com.dbs.hacktron.queuechallenge.component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dbs.hacktron.queuechallenge.vo.Queue;
 
 public interface BaseService {
+	
+	List<Queue> queues = new ArrayList<>();
 
 	public String add(String qName,int maxSize);
 	public void remove(String qName);
