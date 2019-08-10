30a6244c74c7fdea8fdf8fee4c876144aefff476
Jagdish <Jagdish@LAPTOP-MECNDFGM>
2019-08-10 12:07:54 +0530

    service classes

---
+++ src/main/java/com/dbs/hacktron/queuechallenge/component/BaseService.java
@@ -0,0 +1,8 @@
+package com.dbs.hacktron.queuechallenge.component;
+
+public interface BaseService {
+
+	public void add(Object obj);
+	public void remove(Object obj);
+	public void browse(Object obj);
+}
---
+++ src/main/java/com/dbs/hacktron/queuechallenge/component/QueueService.java
@@ -0,0 +1,27 @@
+package com.dbs.hacktron.queuechallenge.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueService implements BaseService{
+
+	
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void browse(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
